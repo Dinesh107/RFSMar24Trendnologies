d75a1a185fcc59f932a61a724e0ded9316db325b
Dinesh107 <dineshjr107@gmail.com>
2024-04-04 12:49:28 +0530

    FifthCommit

---
+++ index.js
@@ -0,0 +1,120 @@
+// console.log("Welcome To JavaScript");
+
+// console.log("It really good language");
+
+// window.alert("I really want to learn javascript");
+
+// let age; // declaration
+
+// age = 20; // assignment or initalization
+
+// let age = 20;  //  declaration + initalization
+
+
+// let firstName = "Rahul"; // string data
+
+// let age = 20;  // numbers data
+
+// let student = true;  // boolean data
+
+// age = age + 1;
+
+// console.log("Hello", firstName);
+
+// console.log("You are", age, "years old");
+
+// console.log("Enrolled:-",student);
+
+
+
+// document.getElementById("p1").innerHTML = "Hello " + firstName
+
+// document.getElementById("p2").innerHTML = "You are " + age +   " Years old";
+
+// document.getElementById("p3").innerHTML = "Enrolled:- " + student;
+
+
+
+let customer = 20;
+
+// customer = customer + 1;
+
+// customer = customer - 1;
+
+// customer = customer * 2;
+
+// customer = customer / 2;
+
+// customer = customer % 2;
+
+// customer += 1; // agumented assignment operators
+
+// customer -= 1;
+
+// customer *= 1;
+
+// customer /= 1;
+
+// customer %= 1;
+
+// console.log(customer);
+
+
+// let username = window.prompt("What is Your Name?");
+
+// console.log("Your name is",username);  
+
+
+// let username;
+
+// document.getElementById("myButton").onclick = function() {
+
+//     username = document.getElementById("myText").value;
+
+//     console.log(username);
+
+//     document.getElementById("myLabel").innerHTML = "Hello " + username;
+
+// }
+
+// let age = window.prompt("How old are you?");
+
+// console.log(typeof age);
+
+// age = Number(age);
+
+// console.log(typeof age);
+
+// age += 1;
+
+// console.log("Happy birthday!!! You are", age, "years old");
+
+// let i;
+
+// let j;
+
+// let k;
+
+// i = Number("3.45");
+
+// j = String(3.43);
+
+// k = Boolean("Mushrooms");
+
+// console.log(i, typeof i);
+// console.log(j, typeof j);
+// console.log(k, typeof k);
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter the radius of the circle");
+
+// radius = Number(radius);
+
+// //9PI = 430.68;
+
+// circumference = 2 * PI * radius;
+
+// console.log("The circumference of the circle is " + circumference);
